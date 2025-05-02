01e74a43faf76aed04a1f36531e2992e38b5d009
jongho6491 <cjh64917@gmail.com>
2025-05-02 14:11:52 +0900

    Add files via upload

---
+++ mysort/Sort.java
@@ -0,0 +1,28 @@
+package sort.mysort;
+
+import java.util.Arrays;
+
+// sort 추상 클래스  (정렬 알고리즘)
+public abstract class Sort {
+	int[] orgData;
+	int[] sortedData;
+	
+	public abstract void sort();
+	
+	// setData 를 입력받아서 orgData로 넣는 역할
+	public void setData(int[] datalist) {
+		this.orgData = datalist;
+		this.sortedData = datalist.clone();  // shallow copy(얇은 복사) / deep copy(깊은 복사)
+											 //	-> .clone();
+	}
+	
+	public String getOrgData() {
+		return Arrays.toString(orgData);
+	}
+	
+	public String getSortedData() {
+		return Arrays.toString(sortedData);
+	}
+	
+	
+}
